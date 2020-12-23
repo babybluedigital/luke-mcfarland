8498810fc6c6f4dee590575a6d51c47971b62e22
Baby Blue <luke.mcfarland@babyblue.info>
2020-12-23 13:15:29 +0200

    Family Tree Page Added
    Framework for Family Tree Added

---
+++ javascript/my.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.genealogy-tree ul').hide();
+    $('.genealogy-tree>ul').show();
+    $('.genealogy-tree ul.active').show();
+    $('.genealogy-tree li').on('click', function (e) {
+        var children = $(this).find('> ul');
+        if (children.is(":visible")) children.hide('fast').removeClass('active');
+        else children.show('fast').addClass('active');
+        e.stopPropagation();
+    });
+});
